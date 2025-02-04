b84ec636179128cb96ab5a217f3d05093f8cb3c0
ViktorKorneplodd <150427942+ViktorKorneplodd@users.noreply.github.com>
2025-02-04 21:28:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 import uuid
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key_here'  # Замените на свой секретный ключ
+app.secret_key = 'your_secret_key_here' 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     'mssql+pyodbc://DESKTOP-19QLP36\MSSQLSERVERRR/DIPLOM?driver=ODBC+Driver+17+for+SQL+Server&Trusted_Connection=yes'
 )
@@ -14,7 +14,7 @@ db = SQLAlchemy(app)
 class Tovar(db.Model):
     __tablename__ = 'Tovar'
     id = db.Column(db.Integer, primary_key=True)
-    unique_id = db.Column(db.String(36), unique=True)  # Уникальный идентификатор
+    unique_id = db.Column(db.String(36), unique=True)  
     Название = db.Column(db.String(255))
     Описание = db.Column(db.Text)
     Цена = db.Column(db.Float)
@@ -22,30 +22,25 @@ class Tovar(db.Model):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.unique_id = str(uuid.uuid4())  # Генерация уникального идентификатора
+        self.unique_id = str(uuid.uuid4()) 
 
 @app.route('/')
 def home():
-    товары = Tovar.query.all()  # Получаем все товары из базы данных
+    товары = Tovar.query.all()  
 
     if not товары:
         return render_template('index.html', товар=None)
 
-    # Получаем список уже показанных товаров из сессии
     shown_ids = session.get('shown_ids', [])
 
-    # Находим доступные товары
     available_tovars = [t for t in товары if t.id not in shown_ids]
 
     if not available_tovars:
-        # Если все товары были показаны, сбрасываем список
         shown_ids = []
         available_tovars = товары
 
-    # Выбираем случайный товар
     случайный_товар = random.choice(available_tovars)
 
-    # Добавляем ID выбранного товара в список показанных
     shown_ids.append(случайный_товар.id)
     session['shown_ids'] = shown_ids
 
